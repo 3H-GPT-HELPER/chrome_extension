b7ce959138f2ddcb925ed78563ac7d100ad4f4ad
oh-bom <spdlqj888888@gmail.com>
2023-10-11 15:01:12 +0900

    [feat]:사이트 로그인 상태 통신

--- popup.js
+++ popup.js
@@ -1,37 +1,34 @@
 
-
-// import axios from './node_modules/axios';
-// import cheerio from './node_modules/cheerio';
-
-
 document.addEventListener('DOMContentLoaded', () => {
     let btn = document.getElementById('btn');
-    // chrome.storage.sync.get('color', ({ color }) => {
-    //   btn.style.backgroundColor = color;
-    // });
 
-    function setPageBackgroundColor() {
-      chrome.storage.sync.get('color', ({ color }) => {
-        document.body.style.backgroundColor = color;
-      });
-    }
-  
-    //popup.js test(url 획득)
-    // btn.addEventListener('click', async () => {
-    //   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    //   var url='';
-    //   getTabUrl().then(function(url){
-    //     console.log("~~"+url);
-       
-    //   });
+    //login 여부 확인 check
+    fetch('http://127.0.0.1:8000/proxy', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          'X-CSRFToken': getCookie('csrftoken'),
+      },
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.authenticated=='True') {
+          // 사용자가 로그인되어 있는 경우
+          const username = data.username;
+          console.log('login',username)
+          alert(username+" is logged in:) ");
+          // username을 사용하여 원하는 작업 수행
+      } else {
+          console.log("not login")
+          //notLoginPopUp();
+          alert("Not Login! login in helper site first !\n http://127.0.0.1:8000/ ");
+      }
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
 
-    //   //btn color change
-    //   //scripting은 현재 페이지의 js를 고칠때 사용!
-    //   chrome.scripting.executeScript({
-    //     target: { tabId: tab.id },
-    //     function: setPageBackgroundColor,
-    //   });
-    // });
+    // end check login 
 
     //html에 접근
     let helperBtn=document.getElementById('helperBtn');
@@ -43,10 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
         function(response){
           alert('chatGpt Helper ON!');
           console.log(response);
-        
-          
-        }
-        );
+
+        });
 
         
 
@@ -54,6 +49,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
   });
+
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  };
 
 // 현재 윈도우 화면에서 URL 정보를 얻어오는 함수
 // function getTabUrl(callback) {
@@ -92,3 +102,13 @@ function renderUrl(urlText){
   console.log(urlText);
 }
 
+
+function notLoginPopUp(){
+  var popup = window.open('', '', 'width=400,height=200');
+  popup.document.write('<html><head><title>알림</title></head><body>');
+  popup.document.write('<p>Not Login! Login in helper site first! </p>');
+  popup.document.write('<a href="http://127.0.0.1:8000/">http://127.0.0.1:8000/</a>');
+  popup.document.write('</body></html>');
+  popup.document.close();
+
+}
